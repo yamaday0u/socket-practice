ef6810e866c552274415ad890b8e568aefccad93
yamaday0u <35hallelujah00@gmail.com>
2023-06-17 23:09:08 +0900

    [add] interactive interface

--- client/index.ts
+++ client/index.ts
@@ -6,4 +6,27 @@ const socket = io(`http://localhost:${port}`, { path: "/socket/" });
 
 socket.on("connect", () => {
   console.log(`client connected on port ${port}`);
+  console.log(`client connected ${socket.connected}`);
+  startMessaging();
 });
+
+import readLine from "readline";
+const readInterface = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const startMessaging = () => {
+  const inputMessage = function() {
+    readInterface.question(
+    "> ",
+    (input: string) => {
+      socket.emit("client", input, (response: any) => {
+        inputMessage();
+      });
+    });
+  }
+  console.log("メッセージを入力してください");
+  inputMessage();
+}
+
--- server/event.ts
+++ server/event.ts
@@ -3,5 +3,10 @@ import socketIo from "socket.io";
 export const event = function(io: socketIo.Server) {
   io.on("connect", (socket) => {
     console.log("server connected");
+
+    socket.on("client", (message: string, callback: any) => {
+      console.log("received message: " + message);
+      callback({ status: "OK" });
+    });
   });
 }
